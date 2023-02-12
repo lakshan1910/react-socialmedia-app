6ba528cfd3869e91d5ef45dd81dc2772de779464
Your Name <you@example.com>
2023-02-12 09:59:19 +0530

    topbar created

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,9 +1,48 @@
-// import 'topbar.css'
+ import './topbar.css'
+ import SearchIcon from '@mui/icons-material/Search';
+ import PersonIcon from '@mui/icons-material/Person';
+ import ChatIcon from '@mui/icons-material/Chat';
+ import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar() {
   return (
-    <div>
-      topbar
+    <div className='topbarContainer'>
+      <div className="topbarLeft">
+        <span className='logo'>LAKSHAN SOCIAL</span>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchbar">
+        <SearchIcon className="searchIcon"/>
+        <input placeholder='Search for friend, post or video' className='searchInputs' />
+        </div>
+      </div>
+      
+      <div className="topbarRight">
+        <div className="topbarLinks">
+            <span className='topbarLink'>HomePage</span>
+            <span className='topbarLink'>Timeline</span>
+        </div>
+        <div className="topbarIcons">
+            <div className="topbarIconItem">
+                <PersonIcon/>
+                <span className='topbarIconBadge'>1</span>
+            </div>
+        </div>
+        <div className="topbarIcons">
+            <div className="topbarIconItem">
+                <ChatIcon/>
+                <span className='topbarIconBadge'>2</span>
+            </div>
+        </div>
+        <div className="topbarIcons">
+            <div className="topbarIconItem">
+                <NotificationsIcon/>
+                <span className='topbarIconBadge'>4</span>
+            </div>
+        </div>
+
+        <img src="/assets/person/1.jpg" alt="" className='topbarImg'/>      </div>
     </div>
   )
 }
